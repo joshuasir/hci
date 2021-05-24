755e619e071a5d1b114e45bc25158e2fdc0c8b66
joshua <josuasirustara@gmail.com>
2021-05-24 14:32:42 +0700

    added arrow

--- slider.js
+++ slider.js
@@ -1,19 +1,35 @@
+var curr = 1;
+var $wrapper = $('.slides', '#slider');
+var $slider = $('.slide', '#slider');
+
+function next() {
+    $wrapper.animate({
+        'margin-left': '-=' + 1420
+    }, 1000, function() {
+        if (++curr === $slider.length) {
+            curr = 1;
+            $wrapper.css('margin-left', 0);
+        }
+    });
+}
+
+function prev() {
+    $wrapper.animate({
+        'margin-left': '+=' + 1420
+    }, 1000, function() {
+        if (++curr === $slider.length) {
+            curr = 1;
+            $wrapper.css('margin-left', 0);
+        }
+    });
+}
 $(function() {
-    var curr = 1;
-    var $wrapper = $('.slides', '#slider');
-    var $slider = $('.slide', '#slider');
+
     var interval;
 
     function init() {
         interval = setInterval(function() {
-            $wrapper.animate({
-                'margin-left': '-=' + 1420
-            }, 1000, function() {
-                if (++curr === $slider.length) {
-                    curr = 1;
-                    $wrapper.css('margin-left', 0);
-                }
-            });
+            next();
         }, 4000);
     }
 
